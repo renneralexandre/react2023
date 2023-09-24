822be7ec1785c24f9b35e883a5f534d17bc9782a
renneralexandre <renner.alexandre@gmail.com>
2023-09-24 11:12:40 -0300

    feature: router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import theme from "./theme";
-import App from "./App.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import theme from "./theme/index";
+// import App from "./App.tsx";
 // ======================= =======================
+const router = createBrowserRouter([{ path: "/", element: <div>rota</div> }]);
 
 // ======================= =======================
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    {/* <ThemeProvider theme={theme}> */}
+    <CssBaseline />
+    <RouterProvider router={router} />
+    {/* </ThemeProvider> */}
   </React.StrictMode>
 );
