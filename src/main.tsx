47bc1ae0744540b098c2d2a7af38f0044e798a75
renneralexandre <renner.alexandre@gmail.com>
2023-09-24 10:57:52 -0300

    refactor: theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme";
 import App from "./App.tsx";
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-import { blue, red } from "@mui/material/colors";
 // ======================= =======================
-
-// import red from '@material'
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      dark: blue[900],
-      main: blue[800],
-      light: blue[700],
-    }
-  }
-});
-theme.palette.primary.main = red[700];
-
 
 // ======================= =======================
 ReactDOM.createRoot(document.getElementById("root")!).render(
