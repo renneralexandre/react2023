b87074df3230927653ac650199a92db49630912b
renneralexandre <renner.alexandre@gmail.com>
2023-10-22 11:57:59 -0300

    feature: clear old files

--- src/components/ResponsiveAppBar/index.tsx
+++ src/components/ResponsiveAppBar/index.tsx
@@ -1,11 +1,20 @@
-import { AppBar, Button, Toolbar } from "@mui/material";
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+
+import AccountCircleTwoToneIcon from "@mui/icons-material/AccountCircleTwoTone";
+import LoginTwoToneIcon from "@mui/icons-material/LoginTwoTone";
 
 const ResponsiveAppBar = () => {
   const navigate = useNavigate();
+  const isAuthenticated = false;
   return (
     <AppBar position="static">
-      <Toolbar>
+      <Toolbar disableGutters>
+        <AccountCircleTwoToneIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Meu Aplicativo
+        </Typography>
         <Button variant="menu2" onClick={() => navigate("/")}>
           Home
         </Button>
@@ -15,6 +24,11 @@ const ResponsiveAppBar = () => {
         <Button variant="menu2" onClick={() => navigate("/form")}>
           Form
         </Button>
+        {isAuthenticated ? (
+          <Typography variant="body2">LOGADO</Typography>
+        ) : (
+          <LoginTwoToneIcon fontSize="large" color="action" sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+        )}
       </Toolbar>
     </AppBar>
   );
